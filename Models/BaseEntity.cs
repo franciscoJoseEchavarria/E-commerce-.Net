48b29abe9bb1d81ca3cad9ea2b485636115eb28f
Francisco Echavarría <fjepaez@gmail.com>
2025-04-26 13:17:24 -0500

    before the implement microservices in this project and arquitecture clean

--- Models/BaseEntity.cs
+++ Models/BaseEntity.cs
@@ -8,8 +8,15 @@ namespace NuevoProyecto.API.Models
     public abstract class BaseEntity
     {
         public int Id { get; set; }
-        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
-        public DateTime? UpdatedAt { get; set; }
+        public DateTime CreatedAt { get; set; } = TimeZoneInfo.ConvertTimeFromUtc(
+            DateTime.UtcNow, 
+            TimeZoneInfo.FindSystemTimeZoneById("SA Pacific Standard Time")
+        );
+        public DateTime? UpdatedAt { get; set; } = TimeZoneInfo.ConvertTimeFromUtc(
+            DateTime.UtcNow, 
+            TimeZoneInfo.FindSystemTimeZoneById("SA Pacific Standard Time")
+        );
+        
         
         // Método abstracto que todas las entidades deben implementar
         public abstract bool IsValid();
